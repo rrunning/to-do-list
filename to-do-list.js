3ab36d6458ad4a80b00be29638f9e370ad52f7ee
Ryan Running <ryanrunning@securitymetrics.com>
2016-09-26 22:25:15 -0600

    trying new things with javascript file

--- to-do-list.js
+++ to-do-list.js
@@ -1,10 +1,6 @@
 
-
-
 function addItem() {
-	var item = getElementById('item').value;
-	var element = document.createElement('li');
-	item.appendChild(element)
-	document.getElementById('list').appendChild(element)
-
+	var element = document.createElement("li");
+	document.body.appendChild(element);
+	var input = getElementById('item').value;
 }
